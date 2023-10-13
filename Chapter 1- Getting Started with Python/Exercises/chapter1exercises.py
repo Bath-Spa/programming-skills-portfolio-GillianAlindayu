a62bee39d99f3f2240b32a528ebf139ade5972e0
Gillian Alindayu <alindayu.gillianmarie2020@gmail.com>
2023-10-13 09:49:36 +0400

    chapter1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/chapter1exercises.py
@@ -0,0 +1,37 @@
+# create string type variables 
+
+name = "python"
+print ("name")
+
+message = "I love python"
+print(message)
+
+# This program prints Hello, World! 
+
+print("hello world")
+
+# this programs add 2 numbers 
+
+num1 = 1.5
+num2 = 6.3
+
+# add two numbers 
+
+sum = num1 + num2 
+
+# display the sum 
+
+print('the sum of {1} is {2}'.format(num1, num2, sum))
+
+# python program to calculate the square root 
+# Note: change the value of different result
+
+num = 8 
+
+# to take input from the user 
+
+num = float(input('Enter the number'))
+
+num_sqrt = num  ** 5 
+print('the square root of %0.3f% is %0.3f%'(num ,num_sqrt))
+
