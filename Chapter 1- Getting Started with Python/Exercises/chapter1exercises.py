0c25d82408cf8bb4f646c6d252c10b47f38f62eb
Gillian Alindayu <alindayu.gillianmarie2020@gmail.com>
2023-10-24 12:38:33 +0400

    Update chapter1exercises.py

--- Chapter 1- Getting Started with Python/Exercises/chapter1exercises.py
+++ Chapter 1- Getting Started with Python/Exercises/chapter1exercises.py
@@ -1,37 +1,5 @@
-# create string type variables 
+#exercise 1: print strings: ballot_box_with a check: 
+#creating string types variable 
 
-name = "python"
-print ("name")
-
-message = "I love python"
-print(message)
-
-# This program prints Hello, World! 
-
-print("hello world")
-
-# this programs add 2 numbers 
-
-num1 = 1.5
-num2 = 6.3
-
-# add two numbers 
-
-sum = num1 + num2 
-
-# display the sum 
-
-print('the sum of {1} is {2}'.format(num1, num2, sum))
-
-# python program to calculate the square root 
-# Note: change the value of different result
-
-num = 8 
-
-# to take input from the user 
-
-num = float(input('Enter the number'))
-
-num_sqrt = num  ** 5 
-print('the square root of %0.3f% is %0.3f%'(num ,num_sqrt))
+print('twinkle twinkle little star, \n\t how I wonder what you are! \n\t\t Up above the world so high \n\t\t like a diamond in the sky. \ntwinkle, twinkle, little star \n How I wonder what you are!')
 
