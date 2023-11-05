9aea5e0da0bdadfcd28f7b6cad9aa27925bdc443
Gillian Alindayu <144450514+GillianAlindayu@users.noreply.github.com>
2023-11-05 20:35:02 +0400

    version 1: practice program

---
+++ practiceprogram1.py
@@ -0,0 +1,27 @@
+# this program is for creating variable 
+
+
+print ("this is test")
+print(10/3)
+
+
+# this program is for GENERAL VARIABLE and Printing it 
+age = 18 
+print ("you are", age, "years old")
+
+
+# this program is for CONCATINATION 
+a = "hello"
+b = "Big"
+c = "World"
+print(a, " " + b, " " + c)
+
+
+# this program uses REPITITION 
+print(a*3)
+message = "hello" + "world"
+print("message")
+print ('enter the amount of ' + 'sales for the day and ' + 'press Enter.' )
+my_str = 'one' 'two' 'three'
+print("my_str")
+'onetwothree'
